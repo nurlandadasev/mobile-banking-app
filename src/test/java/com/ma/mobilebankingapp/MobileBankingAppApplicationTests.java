54e46ce5b9fa8c994eeb221cbb506afd3c565dc4
Nurlan Dadashov <nurlandadasev@gmail.com>
2021-05-27 12:54:50 +0300

    Nurlan

---
+++ src/test/java/com/ma/mobilebankingapp/MobileBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ma.mobilebankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileBankingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
